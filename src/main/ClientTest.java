f816775b9d5887219cb5f99a0d7936e6c0ca690d
Enes Tasbasi <enestasbasi@outlook.com>
2016-05-15 20:07:20 -0400

    fix the host issue

--- src/main/ClientTest.java
+++ src/main/ClientTest.java
@@ -10,7 +10,7 @@ public class ClientTest {
 
 	public static void main(String[] args) {
 		Client charlie;
-		charlie = new Client("127.0.0.1");
+		charlie = new Client("25.4.111.12");
 		charlie.setLocationRelativeTo(null);
 		charlie.setDefaultCloseOperation(charlie.EXIT_ON_CLOSE);
 		charlie.startRunnign();
